0435dca81d720030ddec456de3555adb0cd5603d
Kevin Kuehler <Kevin.Kuehler@viasat.com>
2018-06-15 15:30:12 -0700

    add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="omniidl",
+    version="4.2.2",
+    author="Kevin Kuehler",
+    author_email="Kevin.Kuehler@viasat.com",
+    description="omniidl library for writing backends",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://omniorb.sourceforge.net/",
+    packages=find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU Lesser General Public License v2.1",
+        "Operating System :: OS Independent",
+    ),
+)
